bfd250bffd52aaa651b886b317747b80cf2bab22
clarkdjcr <clarkdjcr@hotmail.com>
2025-09-10 23:58:54 -0400

    Initial Commit

---
+++ FoodRecovery/FoodRecoveryApp.swift
@@ -0,0 +1,32 @@
+//
+//  FoodRecoveryApp.swift
+//  FoodRecovery
+//
+//  Created by Donald Clark on 9/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FoodRecoveryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FoodRecovery/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FoodRecovery
+//
+//  Created by Donald Clark on 9/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
